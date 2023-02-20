76c63598a6650a318e224980a88b561b22a3db28
minwoo <105141296+ko0or@users.noreply.github.com>
2023-02-20 13:49:46 +0900

    Add files via upload

---
+++ 20230220/JavaProject/src/HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld {
+/*
+	About -->
+		작성일: 2023 02 20
+		작성자: 홍길동
+		내용	: HelloWorld 클래스로 문자열 출력
+
+	
+ */
+
+	public static void main(String[] args) {
+		// 타입 -> String
+		// 변수명 -> message
+		
+		/* 
+		변수선언
+		String message;
+		*/
+		
+
+		// 변수초기화
+		String message = "헬로월드!"; 
+		
+		
+		// 출력
+		System.out.println( message );
+		
+		
+		
+	}
+}
